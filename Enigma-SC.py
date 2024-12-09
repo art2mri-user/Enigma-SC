16753c5ebfc2f0a1e76a318b6048aa17b7a2ce1e
art2mri-user <150076332+art2mri-user@users.noreply.github.com>
2024-12-09 15:03:30 -0300

    Update Enigma-SC.py

--- Enigma-SC.py
+++ Enigma-SC.py
@@ -636,8 +636,8 @@ def docker():
 
 
 	for idx,i in enumerate(file_paths):
-		command53='docker cp '+str(i)+'/'+os.path.basename(str(i))+'.nii.gz vertebral_labeling:/home/datav2/nnUNet_raw/Dataset791_SCT/imagesTs/'+os.path.basename(str(i))+'_0000.nii.gz'
-		command54='sudo -S docker cp '+str(i)+'/'+os.path.basename(str(i))+'.nii.gz vertebral_labeling:/home/datav2/nnUNet_raw/Dataset791_SCT/imagesTs/'+os.path.basename(str(i))+'_0000.nii.gz'
+		command53='docker cp '+str(i)+'/'+os.path.basename(str(i))+'.nii.gz vertebral_labeling:/home/datav2/nnUNet_raw/Dataset761_SCT/imagesTs/'+os.path.basename(str(i))+'_0000.nii.gz'
+		command54='sudo -S docker cp '+str(i)+'/'+os.path.basename(str(i))+'.nii.gz vertebral_labeling:/home/datav2/nnUNet_raw/Dataset761_SCT/imagesTs/'+os.path.basename(str(i))+'_0000.nii.gz'
 		os.system(command53)
 		command55='docker start vertebral_labeling'
 		command56='docker start vertebral_labeling'
@@ -649,7 +649,7 @@ def docker():
 		comando_12 = 'docker exec -it vertebral_labeling python3 /home/scripts/cuda.py'
 		comando_2 = 'docker exec -it vertebral_labeling python3 /home/scripts/cpu.py'
 		command_1 = 'docker exec -it vertebral_labeling python3 /home/scripts/cuda.py'
-		file_to_check = '/home/datav2/inference/791_SCT/preds/'+os.path.basename(str(i))+'.nii.gz'
+		file_to_check = '/home/datav2/inference/761_SCT/preds/'+os.path.basename(str(i))+'.nii.gz'
 		command_21 = 'docker exec -it vertebral_labeling python3 /home/scripts/cpu.py'
 		command_22 = 'docker exec -it vertebral_labeling python3 /home/scripts/cpu.py'
 		exit16 = subprocess.run(comando_11, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
@@ -675,14 +675,14 @@ def docker():
 				os.system('docker exec -it vertebral_labeling python3 /home/scripts/cpu.py')
 			else:
 				os.system('docker exec -it vertebral_labeling python3 /home/scripts/cpu.py')
-		command555='docker exec -it vertebral_labeling chmod -R 777 /home/datav2/inference/791_SCT/preds/'+os.path.basename(str(i))+'.nii.gz'
-		command666='docker exec -it vertebral_labeling chmod -R 777 /home/datav2/inference/791_SCT/preds/'+os.path.basename(str(i))+'.nii.gz'
+		command555='docker exec -it vertebral_labeling chmod -R 777 /home/datav2/inference/761_SCT/preds/'+os.path.basename(str(i))+'.nii.gz'
+		command666='docker exec -it vertebral_labeling chmod -R 777 /home/datav2/inference/761_SCT/preds/'+os.path.basename(str(i))+'.nii.gz'
 		os.system(command555)
-		command515='docker cp vertebral_labeling:/home/datav2/inference/791_SCT/preds/'+os.path.basename(str(i))+'.nii.gz'+' '+str(i)+'/'+os.path.basename(str(i))+'_seg_labeled.nii.gz'
-		command616='sudo docker cp vertebral_labeling:/home/datav2/inference/791_SCT/preds/'+os.path.basename(str(i))+'.nii.gz'+' '+str(i)+'/'+os.path.basename(str(i))+'_seg_labeled.nii.gz'
+		command515='docker cp vertebral_labeling:/home/datav2/inference/761_SCT/preds/'+os.path.basename(str(i))+'.nii.gz'+' '+str(i)+'/'+os.path.basename(str(i))+'_seg_labeled.nii.gz'
+		command616='sudo docker cp vertebral_labeling:/home/datav2/inference/761_SCT/preds/'+os.path.basename(str(i))+'.nii.gz'+' '+str(i)+'/'+os.path.basename(str(i))+'_seg_labeled.nii.gz'
 		os.system(command515)
-		command575='docker exec -it vertebral_labeling rm -f /home/datav2/nnUNet_raw/Dataset791_SCT/imagesTs/'+os.path.basename(str(i))+'_0000.nii.gz'
-		command676='sudo docker exec -it vertebral_labeling rm -f /home/datav2/nnUNet_raw/Dataset791_SCT/imagesTs/'+os.path.basename(str(i))+'_0000.nii.gz'
+		command575='docker exec -it vertebral_labeling rm -f /home/datav2/nnUNet_raw/Dataset761_SCT/imagesTs/'+os.path.basename(str(i))+'_0000.nii.gz'
+		command676='sudo docker exec -it vertebral_labeling rm -f /home/datav2/nnUNet_raw/Dataset761_SCT/imagesTs/'+os.path.basename(str(i))+'_0000.nii.gz'
 		os.system(command575)
 					
 		os.system('docker cp '+str(i)+'/'+os.path.basename(str(i))+'.nii.gz vertebral_labeling:/'+os.path.basename(str(i))+'.nii.gz')
@@ -804,12 +804,12 @@ def singularity():
     		raise SystemExit(1)		
 	password = None
 	
-	k = os.listdir(enigma_folder+'/vertebral_labeling.simg/home/datav2/nnUNet_raw/Dataset791_SCT/imagesTs/')
+	k = os.listdir(enigma_folder+'/vertebral_labeling.simg/home/datav2/nnUNet_raw/Dataset761_SCT/imagesTs/')
 	for i in k:
-		if os.path.isdir(enigma_folder+'/vertebral_labeling.simg/home/datav2/nnUNet_raw/Dataset791_SCT/imagesTs/'+str(i)):
-			shutil.rmtree(enigma_folder+'/vertebral_labeling.simg/home/datav2/nnUNet_raw/Dataset791_SCT/imagesTs/'+str(i))
+		if os.path.isdir(enigma_folder+'/vertebral_labeling.simg/home/datav2/nnUNet_raw/Dataset761_SCT/imagesTs/'+str(i)):
+			shutil.rmtree(enigma_folder+'/vertebral_labeling.simg/home/datav2/nnUNet_raw/Dataset761_SCT/imagesTs/'+str(i))
 		else:
-			os.remove(enigma_folder+'/vertebral_labeling.simg/home/datav2/nnUNet_raw/Dataset791_SCT/imagesTs/'+str(i))	
+			os.remove(enigma_folder+'/vertebral_labeling.simg/home/datav2/nnUNet_raw/Dataset761_SCT/imagesTs/'+str(i))	
 	path = enigma_folder+'/vertebral_labeling.simg/home'
 	dir_list = os.listdir(path)		
 	for i in dir_list:
@@ -822,9 +822,9 @@ def singularity():
 					os.system(command21)
 					
 	for idx,i in enumerate(file_paths):
-		if any(arq.endswith('.nii.gz') for arq in os.listdir('vertebral_labeling.simg/home/datav2/nnUNet_raw/Dataset791_SCT/imagesTs/')):
-			command219='rm vertebral_labeling.simg/home/datav2/nnUNet_raw/Dataset791_SCT/imagesTs/*nii.gz'
-			command222='sudo -S rm vertebral_labeling.simg/home/datav2/nnUNet_raw/Dataset791_SCT/imagesTs/*nii.gz'
+		if any(arq.endswith('.nii.gz') for arq in os.listdir('vertebral_labeling.simg/home/datav2/nnUNet_raw/Dataset761_SCT/imagesTs/')):
+			command219='rm vertebral_labeling.simg/home/datav2/nnUNet_raw/Dataset761_SCT/imagesTs/*nii.gz'
+			command222='sudo -S rm vertebral_labeling.simg/home/datav2/nnUNet_raw/Dataset761_SCT/imagesTs/*nii.gz'
 			exit238 = subprocess.run(command219, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
 			os.system(command219)	
 		#update_progress_bar(idx + 1, len(file_paths))
@@ -834,9 +834,9 @@ def singularity():
 		command69='chmod -R 777 '+enigma_folder+'/'+os.path.basename(str(i))+'.nii.gz'
 		command70='sudo chmod -R 777 '+enigma_folder+'/'+os.path.basename(str(i))+'.nii.gz'
 		os.system(command69)
-		os.system('mv '+os.path.basename(str(i))+'.nii.gz '+'vertebral_labeling.simg/home/datav2/nnUNet_raw/Dataset791_SCT/imagesTs/'+os.path.basename(str(i))+'_0000.nii.gz')	
-		command73='chmod -R 777 vertebral_labeling.simg/home/datav2/nnUNet_raw/Dataset791_SCT/imagesTs/'+os.path.basename(str(i))+'_0000.nii.gz'
-		command74='sudo chmod -R 777 vertebral_labeling.simg/home/datav2/nnUNet_raw/Dataset791_SCT/imagesTs/'+os.path.basename(str(i))+'_0000.nii.gz'
+		os.system('mv '+os.path.basename(str(i))+'.nii.gz '+'vertebral_labeling.simg/home/datav2/nnUNet_raw/Dataset761_SCT/imagesTs/'+os.path.basename(str(i))+'_0000.nii.gz')	
+		command73='chmod -R 777 vertebral_labeling.simg/home/datav2/nnUNet_raw/Dataset761_SCT/imagesTs/'+os.path.basename(str(i))+'_0000.nii.gz'
+		command74='sudo chmod -R 777 vertebral_labeling.simg/home/datav2/nnUNet_raw/Dataset761_SCT/imagesTs/'+os.path.basename(str(i))+'_0000.nii.gz'
 		os.system(command73)
 		try:
 			subprocess.run(["singularity --version"], check=True, shell=True)
@@ -851,7 +851,7 @@ def singularity():
 					subprocess.run(command_12, shell=True)  
 				except subprocess.CalledProcessError:
 					pass
-			dire = 'vertebral_labeling.simg/home/datav2/inference/791_SCT/preds/'
+			dire = 'vertebral_labeling.simg/home/datav2/inference/761_SCT/preds/'
 			file1 = os.path.basename(str(i))+'.nii.gz'
 			way = os.path.join(dire, file1)	     				
 			if os.path.exists(way):
@@ -867,13 +867,13 @@ def singularity():
 		except subprocess.CalledProcessError:
 			print('AUTOMATED LABELING FAILED')			
 
-		command75='chmod -R 777 vertebral_labeling.simg/home/datav2/inference/791_SCT/preds/'+os.path.basename(str(i))+'.nii.gz'
-		command76='sudo chmod -R 777 vertebral_labeling.simg/home/datav2/inference/791_SCT/preds/'+os.path.basename(str(i))+'.nii.gz'			
+		command75='chmod -R 777 vertebral_labeling.simg/home/datav2/inference/761_SCT/preds/'+os.path.basename(str(i))+'.nii.gz'
+		command76='sudo chmod -R 777 vertebral_labeling.simg/home/datav2/inference/761_SCT/preds/'+os.path.basename(str(i))+'.nii.gz'			
 		os.system(command75)
-		os.system('mv -v '+enigma_folder+'/vertebral_labeling.simg/home/datav2/inference/791_SCT/preds/'+os.path.basename(str(i))+'.nii.gz '+enigma_folder+'/vertebral_labeling.simg/home/datav2/inference/791_SCT/preds/'+os.path.basename(str(i))+'_seg_labeled.nii.gz')
-		os.system('rm vertebral_labeling.simg/home/datav2/nnUNet_raw/Dataset791_SCT/imagesTs/*nii.gz')
+		os.system('mv -v '+enigma_folder+'/vertebral_labeling.simg/home/datav2/inference/761_SCT/preds/'+os.path.basename(str(i))+'.nii.gz '+enigma_folder+'/vertebral_labeling.simg/home/datav2/inference/761_SCT/preds/'+os.path.basename(str(i))+'_seg_labeled.nii.gz')
+		os.system('rm vertebral_labeling.simg/home/datav2/nnUNet_raw/Dataset761_SCT/imagesTs/*nii.gz')
 				
-		os.system('mv '+enigma_folder+'/'+'vertebral_labeling.simg/home/datav2/inference/791_SCT/preds/'+os.path.basename(str(i))+'_seg_labeled.nii.gz '+enigma_folder)			
+		os.system('mv '+enigma_folder+'/'+'vertebral_labeling.simg/home/datav2/inference/761_SCT/preds/'+os.path.basename(str(i))+'_seg_labeled.nii.gz '+enigma_folder)			
 		os.system('mv '+enigma_folder+'/'+os.path.basename(str(i))+'_seg_labeled.nii.gz '+str(i))
 		
 		os.system('cp '+str(i)+'/'+os.path.basename(str(i))+'.nii.gz '+enigma_folder+'/vertebral_labeling.simg/'+os.path.basename(str(i))+'.nii.gz')
@@ -1363,8 +1363,8 @@ def reg_aut_singularity():
 				
 	for idx, i in enumerate(file_paths):
 		if any(arq.endswith('.nii.gz') for arq in os.listdir('vertebral_labeling.simg/home/SCT/')):	
-			command219='rm vertebral_labeling.simg/home/datav2/nnUNet_raw/Dataset791_SCT/imagesTs/*nii.gz'
-			command222='sudo -S rm vertebral_labeling.simg/home/datav2/nnUNet_raw/Dataset791_SCT/imagesTs/*nii.gz'
+			command219='rm vertebral_labeling.simg/home/datav2/nnUNet_raw/Dataset761_SCT/imagesTs/*nii.gz'
+			command222='sudo -S rm vertebral_labeling.simg/home/datav2/nnUNet_raw/Dataset761_SCT/imagesTs/*nii.gz'
 			os.system(command219)
 		if any(arq.endswith('.cache') for arq in os.listdir('vertebral_labeling.simg/home/SCT/')):			
 			command219='rm vertebral_labeling.simg/home/SCT/straightening.cache'
